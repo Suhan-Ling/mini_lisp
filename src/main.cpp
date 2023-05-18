28447d54a249cdeb5a200ac7ed16ac7ba22973e5
Xanthene <suhan.ling@foxmail.com>
2023-05-19 02:45:17 +0800

    lv2

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,6 @@ int main() {
             Parser parser(std::move(tokens));               // TokenPtr 不支持复制
             auto value = parser.parse();
             std::cout << value->toString() << std::endl;    // 输出外部表示
-            // for (auto& token : tokens) {
-            //     std::cout << *token << std::endl;
-            // }
         } catch (std::runtime_error& e) {
             std::cerr << "Error: " << e.what() << std::endl;
         }
