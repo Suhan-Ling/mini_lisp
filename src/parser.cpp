9b27ba534e3a7d7b6fafe178e7549f6bb7ec0129
Xanthene <suhan.ling@foxmail.com>
2023-06-16 18:39:30 +0800

    Final version

--- src/parser.cpp
+++ src/parser.cpp
@@ -11,38 +11,28 @@ ValuePtr Parser::parse() {
     if (tokens.empty()) {
         throw SyntaxError("Empty tokens.");
     }
-
     auto token = std::move(tokens.front());
     tokens.pop_front();
-
     if (token->getType() == TokenType::BOOLEAN_LITERAL) {
         auto value = static_cast<BooleanLiteralToken&>(*token).getValue();
         return std::make_shared<BooleanValue>(value);
-        
     } else if (token->getType() == TokenType::NUMERIC_LITERAL) {
         auto value = static_cast<NumericLiteralToken&>(*token).getValue();
         return std::make_shared<NumericValue>(value);
-
     } else if (token->getType() == TokenType::STRING_LITERAL) {
         auto value = static_cast<StringLiteralToken&>(*token).getValue();
         return std::make_shared<StringValue>(value);
-
     } else if (token->getType() == TokenType::IDENTIFIER) {
         auto value = static_cast<IdentifierToken&>(*token).getName();
         return std::make_shared<SymbolValue>(value);
-
     } else if (token->getType() == TokenType::LEFT_PAREN) {
         return parseTails();
-
     } else if (token->getType() == TokenType::QUOTE) {
         return parseQuote(std::string("quote"));
-
     } else if (token->getType() == TokenType::QUASIQUOTE) {
         return parseQuote(std::string("quasiquote"));
-
     } else if (token->getType() == TokenType::UNQUOTE) {
         return parseQuote(std::string("unquote"));
-
     } else {
         throw SyntaxError("Unimplemented.");
     }
@@ -79,10 +69,10 @@ ValuePtr Parser::parseTails() {
 
 ValuePtr Parser::parseQuote(const std::string& str) {
     return std::make_shared<PairValue>(
-      std::make_shared<SymbolValue>(str),
-      std::make_shared<PairValue>(
-          this->parse(),
-          std::make_shared<NilValue>()
-      )
+        std::make_shared<SymbolValue>(str),
+        std::make_shared<PairValue>(
+            this->parse(),
+            std::make_shared<NilValue>()
+        )
     );
 }
